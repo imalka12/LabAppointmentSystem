4946e7c03e7e65c726be220a07188689d20000c2
Imalka Wijerathna <piumiwijerathna1997@gmail.com>
2024-03-14 20:19:50 +0530

    Initial commit

---
+++ src/main/java/com/example/labappointmentsystem/LabAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.labappointmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabAppointmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/labappointmentsystem/LabAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labappointmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
